7a69334b5f2249f8a11e3e0ab910de8dca6eb62c
ye-r1 <ye_r1@daum.net>
2021-02-06 01:35:17 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
-};
+}
 
-function handleHome(req, res) {
-    res.send("Hello from home");
-};
-
-function handleProfile(req, res) {
-    res.send("You are on my profile");
-};
+const handleHome = (req, res) => res.send("Hello from my ass");
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
 
 
