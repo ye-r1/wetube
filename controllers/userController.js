1fc7963b512972fc8d16a059004da1449b944070
ye-r1 <ye_r1@daum.net>
2021-02-07 03:58:22 +0900

    #2.25 More Controlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,11 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
