d1bebfe1a0cbd35facd3068de9bc3db65b940a48
ye-r1 <ye_r1@daum.net>
2021-02-07 03:48:37 +0900

    #2.24 Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,14 @@ export const postJoin = (req, res) => {
     }
 };
 
-export const login = (req, res) => res.render("login");
+export const getLogin = (req, res) => {
+    res.render("login", {pageTitle: "Log In"});
+};
+
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
